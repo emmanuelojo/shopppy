3eaf39a4a98cd12d44236cbba668653e301a4831
emmanuelojo <emmanuelojo1234@gmail.com>
2022-06-22 22:54:37 +0100

    base url renamed

--- src/router/index.ts
+++ src/router/index.ts
@@ -135,7 +135,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_APP_API_URL),
   routes,
 
   scrollBehavior(to, from, savedPosition) {
